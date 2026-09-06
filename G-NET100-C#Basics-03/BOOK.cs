66403f24881978a6daa38461d1b8cadd3ae0b704
original <original@DESKTOP-H4JMMMN>
2026-09-06 15:39:36 +0300

    frist answer

---
+++ G-NET100-C#Basics-03/BOOK.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace G_NET100_C_Basics_03
+{
+    internal class BOOK
+    {
+        public string title;
+        public int pages;
+
+
+    }
+}
--- G-NET100-C#Basics-03/Program.cs
+++ G-NET100-C#Basics-03/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace G_NET100_C_Basics_03
+﻿using System.Drawing;
+using System.Dynamic;
+
+namespace G_NET100_C_Basics_03
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //C# Assignment 
+
+            //1- Create a Book class with a Title(string) and Pages(int). Create a Book object and store it in a variable of type object.Print it.
+            #region object frist answer
+            BOOK obj = new BOOK();
+            obj.title = "The Great Gatsby";
+            obj.pages = 180;
+            Object obj2 = obj;
+
+            Console.WriteLine(obj2);
+
+            #endregion
+
             
+        // 2-   Using the Book class above, print the result of calling ToString(), Equals() (compare book with itself), GetHashCode(), and GetType() on book.
+
+
+
+
+
         }
-    }
+}
 }
